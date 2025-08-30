cc67e5d9e9d07f8b51c6b3d65863101972d95753
FiLoLer Polar <141121171+Xocks@users.noreply.github.com>
2025-08-30 21:07:32 +0800

    级数求和
    # P1035 [NOIP 2002 普及组] 级数求和
    
    ## 题目描述
    
    已知：$S_n= 1+\dfrac{1}{2}+\dfrac{1}{3}+…+\dfrac{1}{n}$。显然对于任意一个整数 $k$，当 $n$ 足够大的时候，$S_n>k$。
    
    现给出一个整数 $k$，要求计算出一个最小的 $n$，使得 $S_n>k$。
    
    ## 输入格式
    
    一个正整数 $k$。
    
    ## 输出格式
    
    一个正整数 $n$。
    
    ## 输入输出样例 #1
    
    ### 输入 #1
    
    ```
    1
    ```
    
    ### 输出 #1
    
    ```
    2
    ```
    
    ## 说明/提示
    
    **【数据范围】**
    
    对于 $100\%$ 的数据，$1\le k \le 15$。
    
    **【题目来源】**
    
    NOIP 2002 普及组第一题

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	//i是总和
+	double  i = 0;
+	//k是输入的数
+	double k = 0;
+	cin >> k ;
+	for (int n = 1; 1; n++) {
+		//n是要找的数
+		i += 1.0 / n;//数字之后加.0直接变为整数除法
+		
+		if (i > k) {
+			cout << n << endl;
+			break;
+				
+		}
+	    
+	}
+    system("pause");
+
+    return 0;
+    
+ 
+}
+
